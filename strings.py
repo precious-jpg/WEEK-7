b2df7cc9ff0369d0cca16eb990a64b5a2842a66d
precious-jpg <precioussedumedi18@gmail.com>
2025-07-01 20:44:03 +0200

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+#Advanced concepts - Strings
+
+message = ' Hello, World! '
+
+print(message.strip()) # Remove leading and trailing whitespace
+print(message.lower()) # Convert all characters to lowercase
+print(message.split(',')) #Split the string into a list based on the coma
+
+#upper method
+#replace method
+  
---
+++ variable.py
@@ -0,0 +1,10 @@
+#Variables
+
+my_variable = 10
+total_count = 0
+user = 'John'
+
+#Invalid
+second_variable = 10
+user_name = 20
+ 
